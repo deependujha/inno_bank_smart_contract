946e801137c5fa88983298550a6e4d2af8d3744f
deependujha <deependujha21@gmail.com>
2022-08-06 14:35:54 +0530

    fixed issues in requests made

--- contracts/Inno_Bank.sol
+++ contracts/Inno_Bank.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.7.0 <0.9.0;
 
 /**
@@ -29,27 +28,33 @@ contract InnoBank {
         return address(this).balance;
     }
     
+
     // To donate money anonymously
     function donateAnonymously() public payable{
         donated[address(0)]+=msg.value;
     }
+
 
     // Donate money with your identity
     function donate() public payable{
         donated[msg.sender]+=msg.value;
     }
 
+    // Make request with your identity
     function makeRequest(string memory name, uint8 age, string memory idea, uint funds)public returns(uint){
         uint count = requestsMade[msg.sender];
         count++;
+        requestsMade[msg.sender]++;
         Requests[msg.sender][count]=Request(name,age,idea,funds);
         return count;
     }
 
 
+    // Make request anonymously
     function makeRequestAnonymously(string memory idea, uint funds)public returns(uint){
         uint count = requestsMade[address(0)];
         count++;
+        requestsMade[address(0)]++;
         Requests[address(0)][count]=Request('',0,idea,funds);
         return count;
     }
