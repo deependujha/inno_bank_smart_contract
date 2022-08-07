50c8b01008e9b9e90447920b9dae15bf139dbfe8
deependujha <deependujha21@gmail.com>
2022-08-07 14:55:39 +0530

    deployed on ropsten testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_ROPSTEN_API_KEY}`,
+      accounts: [`${process.env.ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
 };
