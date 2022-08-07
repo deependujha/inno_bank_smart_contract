4b64b57b9494c00f50ddfcdf01143acc181425dc
deependujha <deependujha21@gmail.com>
2022-08-07 14:40:33 +0530

    modified smart contract as per frontend

--- contracts/Inno_Bank.sol
+++ contracts/Inno_Bank.sol
@@ -11,11 +11,8 @@ pragma solidity >=0.7.0 <0.9.0;
 
  struct Request{
     address requestedBy;
-    string name;
-    uint8 age;
     string idea;
     uint funds;
-    bool approved;
     uint support;
 }
 
@@ -27,7 +24,6 @@ contract InnoBank {
     mapping(address => uint256) public donated;
     mapping(address => uint256) public received;
     mapping(uint256 => Request) private requestDetail;
-    mapping(address=>mapping(uint=>Request)) private requests;
     mapping(address=>mapping(uint=>bool)) private voted;
 
 
@@ -53,31 +49,32 @@ contract InnoBank {
         donated[msg.sender]+=msg.value;
     }
 
-    // make a request. The limit to ask for fund is upto 50 ethers.
-    function makeRequest(string memory name, uint8 age, string memory idea, uint funds)public returns(uint){
-        if(funds>50000000000000000000){funds=50000000000000000000;}
-        reqId++;
-        requests[msg.sender][reqId]=Request(msg.sender,name,age,idea,funds,false,0);
-        requestDetail[reqId]=Request(msg.sender,name,age,idea,funds,false,0);
-        return reqId;
-    }
 
     // make a request anonymously. The limit to ask for fund is upto 50 ethers.
-    function makeRequestAnonymously(string memory idea, uint funds)public returns(uint){
+    function makeRequest(string memory idea, uint funds)public returns(uint){
         if(funds>50000000000000000000){funds=50000000000000000000;}
         reqId++;
-        requests[address(0)][reqId]=Request(msg.sender,'',0,idea,funds,false,0);
-        requestDetail[reqId]=Request(msg.sender,'',0,idea,funds,false,0);
+        requestDetail[reqId]=Request(msg.sender,idea,funds,0);
         return reqId;
     }
 
+
+    // get details of a request id
+    function getRequestDetails(uint _reqId)public view returns(Request memory){
+        return requestDetail[_reqId];
+    }
+
+
+    // transfer money
     function transferMoney(uint _reqId)public payable{
-        require(requestDetail[_reqId].support >= totalVoters/2,"This request doesn't have support of 50% or more voters.");
+        require(requestDetail[_reqId].support > totalVoters/2,"This request doesn't have support of more than 50% voters.");
         require(requestDetail[_reqId].funds <=address(this).balance,"We don't have sufficient balance right now.");
         payable(requestDetail[_reqId].requestedBy).transfer(requestDetail[_reqId].funds);
         delete requestDetail[_reqId];
     }
     
+
+    // vote for a request
     function voteForARequest(uint _reqId)public{
         require(requestDetail[_reqId].funds!=0,"No request exists with this request Id.");
         require(voter[msg.sender]==true,"You are not a voter.");
@@ -88,15 +85,6 @@ contract InnoBank {
             transferMoney(_reqId);
         }
     }
-
-    function getRequestDetails(uint _reqId)public view returns(Request memory){
-        Request memory data = requestDetail[_reqId];
-        if(data.age==0){
-            data.requestedBy=address(0);
-        }
-        return data;
-    }
-
     
     
 }
